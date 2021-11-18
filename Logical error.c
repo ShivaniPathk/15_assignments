bab07d40082114bf7cb1753b52ad4d6b84c9ea9d
ShivaniPathk <93413574+ShivaniPathk@users.noreply.github.com>
2021-11-18 06:48:03 +0530

    Add files via upload

---
+++ Logical error.c
@@ -0,0 +1,12 @@
+	#include <stdio.h>  
+	int main()  
+	{  
+	   int sum=0; 
+	   int k;// variable initialization   
+	   for(k=1;k<=10;k++)
+	   {  
+	       sum=sum+k;    
+	   }  
+	printf("The  value of sum is %d", sum);  
+	    return 0;
+	}
